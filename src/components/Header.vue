bc617510e7cf137b16b16884b83db53d2a7b5a35
Saurabh Gupta <saurabhgupta72dh3@gmail.com>
2024-12-05 03:05:04 +0530

    Added Header

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="nav">
+    <a href="#">Home</a>
+    <a href="#">Add Restaurant</a>
+    <a href="#">Update Restaurants</a>
+    <a href="#">Log Out</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderPage",
+};
+</script>
+
+<style>
+.nav {
+    background: grey;
+    overflow: hidden;
+    
+}
+.nav a{
+    float: left;
+    color: whitesmoke;
+    padding: 14px 16px;
+    text-align: center;
+    font-size: 18px;
+    text-decoration: none;
+    margin-right: 5px;
+}
+.nav a:hover{
+    background-color: rgb(130, 131, 133);
+    color: bisque;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,16 @@
 <template>
+    <HeaderPage />
     <h1>Welcome Home!</h1>
     <a href=""></a>
 </template>
 
 <script>
+import HeaderPage from './Header.vue';
 export default {
     name: 'HomePage',
+    components:{
+        HeaderPage,
+    },
     mounted(){
         let user = localStorage.getItem('user-info');
         if(!user){
