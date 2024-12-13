974a535e79cfc35a007fe9ef14f46e05b5e91468
Saurabh Gupta <saurabhgupta72dh3@gmail.com>
2024-12-13 12:50:07 +0530

    Resturant API setup

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,27 +1,53 @@
 <template>
     <HeaderPage />
     <h1>Welcome Home! , {{name}}</h1>
-    <a href=""></a>
+    <table border="1px solid">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Contact</th>
+            <th>Address</th>
+        </tr>
+        <tr v-for="item in resturants" :key="item.id">
+            <td>{{ item.id }}</td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.contact }}</td>
+            <td>{{ item.address }}</td>
+        </tr>
+    </table>
 </template>
 
 <script>
 import HeaderPage from './Header.vue';
+import axios from 'axios';
 export default {
     name: 'HomePage',
     data(){
         return{
-            name:''
+            name:'',
+            resturants:[]
         }
     },  
     components:{
         HeaderPage,
     },
-    mounted(){
+    async mounted(){
         let user = localStorage.getItem('user-info');
         this.name = JSON.parse(user).name;
         if(!user){
             this.$router.push({name:'SignUp'});
         }
+
+        let result = await axios.get("http://localhost:3000/resturants");
+        this.resturants = result.data;
+
     }
 }
 </script>
+
+<style>
+td,th{
+    height: 40px;
+    width: 150px;
+}
+</style>
